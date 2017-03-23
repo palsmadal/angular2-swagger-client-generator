6d5c9f1e6a9086ea605bb745247c234d34d41874
Robert Mayer <rwlmayer@gmail.com>
2017-03-23 19:23:31 +0100

    Fixed object type 'object' to be returned as 'any'

--- src/generator.js
+++ src/generator.js
@@ -353,6 +353,8 @@ var Generator = (function () {
                      {
                          property.typescriptType = 'Date';
                      }
+                else if (property.type === 'object')
+                    property.typescriptType = 'any';
                 else
                     property.typescriptType = property.type;
 
